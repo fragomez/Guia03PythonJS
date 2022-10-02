a6a9eaae47dbe2aac7251ad8d1852265cbc29741
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-02 11:58:34 -0500

    Ejercicio04

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia3");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let contador = 1, notaPromedio = 0;
-        while(contador <= 5){
-            let nombre = prompt(`Ingrese el nombre del estudiante ${contador}`);
-            let apellido = prompt(`Ingrese el apellido del estudiante ${contador}`);
-            let edad = parseInt(prompt(`Ingrese la edad del estudiante ${contador}`));
-            let nota = parseInt(prompt(`Ingrese la nota del estudiante ${contador}`));
-            notaPromedio += nota;
-            alert(`El estudiante ${nombre} ${apellido} de ${edad} años tiene una nota de ${nota}`);
-            contador++; 
+        let damas = 1, caballeros = 7;
+        while(damas + caballeros < 42){
+            damas++;
+            caballeros++;
         }
-        let promedio = notaPromedio / 5;
-        document.querySelector("#resultado").innerHTML = `La nota promedio es de: ${promedio}`;
+        document.querySelector("#resultado").innerHTML = `La cantidad de damas es: ${damas}`;
+        document.querySelector("#caballeros").innerHTML = `La cantidad de caballeros es: ${caballeros}`
     })
 })
 

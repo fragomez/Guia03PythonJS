081fe5a37a0301d4a94ffe5f33ff5e2e810e4830
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-02 12:10:19 -0500

    Ejercicio05

--- main.js
+++ main.js
@@ -2,19 +2,30 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia3");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let damas = 1, caballeros = 7;
-        while(damas + caballeros < 42){
-            damas++;
-            caballeros++;
+        let contador = 1, aprobados = 0, reprobados = 0;
+        while(contador <= 2){
+            let nota1 = parseInt(prompt(`Ingrese la primera nota del estudiante ${contador}`));
+            let nota2 = parseInt(prompt(`Ingrese la segunda nota del estudiante ${contador}`));
+            let nota3 = parseInt(prompt(`Ingrese la tercera nota del estudiante ${contador}`));
+            let nota4 = parseInt(prompt(`Ingrese la cuarta nota del estudiante ${contador}`));
+            let nota5 = parseInt(prompt(`Ingrese la quinta nota del estudiante ${contador}`));
+            let promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+            if(promedio <3){
+                reprobados++;
+            } else {
+                aprobados++;
+            }
+            contador++;
         }
-        document.querySelector("#resultado").innerHTML = `La cantidad de damas es: ${damas}`;
-        document.querySelector("#caballeros").innerHTML = `La cantidad de caballeros es: ${caballeros}`
+        document.querySelector("#resultado").innerHTML = `Cantidad de alumnos reprobados: ${reprobados}`;
+        document.querySelector("#aprobados").innerHTML = `Cantidad de alumnos aprobados: ${aprobados}`;
     })
 })
 
 function limpiar() {
     document.querySelector("#guia3").reset();
     document.querySelector("#resultado").innerHTML = "";
+    document.querySelector("#aprobados").innerHTML = "";
 }
 
 addEventListener('reset', limpiar);

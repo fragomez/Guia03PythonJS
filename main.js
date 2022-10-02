2c5e3fca0fe53d7762c2a8c1eb39dd02f7d1f88c
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-02 16:06:21 -0500

    Ejercicio10

--- main.js
+++ main.js
@@ -2,155 +2,26 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia3");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let contador = 1, candidatoA = 0, candidatoB = 0, candidatoC = 0;
-        let cantEstudiantes = parseInt(prompt("Ingrese la cantidad de estudiantes: "));
-
-        while (contador <= cantEstudiantes) {
-            let voto = prompt("Ingrese el candidato por el que desea votar (A, B o C)");
-            if (voto == 'A' || voto == 'a') {
-                candidatoA++;
-            } else if (voto == 'B' || voto == 'b') {
-                candidatoB++;
-            } else if (voto == 'C' || voto == 'c') {
-                candidatoC++;
+        let vehiculos = document.querySelector("#vehiculos").value;
+        let contador = 1, autobus = 0, camion = 0, motocicleta = 0, turismo = 0, pasajeros = 0;
+        while(vehiculos >= contador){
+            let vehiculo = prompt("Ingrese el tipo de vehiculo");
+            if (vehiculo == "autobus"){
+                autobus++;
+            } else if (vehiculo == "camion"){
+                camion++;
+            } else if (vehiculo == 'motocicleta'){
+                motocicleta++;
+            } else if (vehiculo = 'turismo'){
+                pasajeros += parseInt(prompt("Cantidad de turistas en el vehiculo: "));
+                turismo++;
             }
             contador++;
         }
-        let votosCandidatoA = (candidatoA / cantEstudiantes) * 100;
-        let votosCandidatoB = (candidatoB / cantEstudiantes) * 100;
-        let votosCandidatoC = (candidatoC / cantEstudiantes) * 100;
-
-        let contador2 = 1;
-        candidatoA = 0, candidatoB = 0, candidatoC = 0;
-        if (votosCandidatoA >= 51) {
-            document.querySelector("#resultado").innerHTML = `El Candidato A ganó con ${candidatoA} votos`
-        } else if (votosCandidatoB >= 51) {
-            document.querySelector("#resultado").innerHTML = `El Candidato B ganó con ${candidatoB} votos`
-        } else if (votosCandidatoC >= 51) {
-            document.querySelector("#resultado").innerHTML = `El Candidato C ganó con ${candidatoC} votos`
-        } else if (votosCandidatoA == votosCandidatoB && votosCandidatoB == votosCandidatoC) {
-            document.querySelector("#resultado").innerHTML = "Hay un triple empate";
-        } else if (votosCandidatoA > votosCandidatoB && votosCandidatoB > votosCandidatoC) {
-            let votosCandidatoA = 0, votosCandidatoB = 0;
-            while (cantEstudiantes >= contador2) {
-                let voto = prompt("Ingrese el candidato por el que desea votar (A o B)");
-                if (voto == 'A' || voto == 'a') {
-                    candidatoA++;
-                } else if (voto == 'B' || voto == 'b') {
-                    candidatoB++;
-                }
-                votosCandidatoA = (candidatoA / cantEstudiantes) * 100;
-                votosCandidatoB = (candidatoB / cantEstudiantes) * 100;
-                contador2++;
-            }
-            if (votosCandidatoA > votosCandidatoB) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato A con ${candidatoA} votos`;
-            } else if (votosCandidatoB > votosCandidatoA) {
-                document.querySelector("#resultado").innerHTML = `El Candidato B ganó con ${candidatoB} votos`
-            } else {
-                document.querySelector("#resultado").innerHTML = "Hay un empate";
-            }
-        } else if (votosCandidatoA > votosCandidatoC && votosCandidatoC > votosCandidatoB) {
-            let votosCandidatoA, votosCandidatoC = 0;
-            while (cantEstudiantes >= contador2) {
-                let voto = prompt("Ingrese el candidato por el que desea votar (A o C)");
-                if (voto == 'A' || voto == 'a') {
-                    candidatoA++;
-                } else if (voto == 'C' || voto == 'C') {
-                    candidatoC++;
-                }
-                votosCandidatoA = (candidatoA / cantEstudiantes) * 100;
-                votosCandidatoC = (candidatoC / cantEstudiantes) * 100;
-                contador2++;
-            }
-            if (votosCandidatoA > votosCandidatoC) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato A con ${candidatoA} votos`;
-            } else if (votosCandidatoC > votosCandidatoA) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato C con ${candidatoC} votos`;
-            } else {
-                document.querySelector("#resultado").innerHTML = "Hay un empate";
-            }
-        } else if (votoCandidatoB > votosCandidatoC && votosCandidatoC > votosCandidatoA) {
-            let votosCandidatoB = 0, votosCandidatoC = 0;
-            while (cantEstudiantes >= contador2) {
-                let voto = prompt("Ingrese el candidato por el que desea votar (B o C)");
-                if (voto == 'B' || voto == 'b') {
-                    candidatoB++;
-                } else if (voto == 'C' || voto == 'c') {
-                    candidatoC++;
-                }
-                votosCandidatoB = (candidatoB / cantEstudiantes) * 100;
-                votosCandidatoC = (candidatoC / cantEstudiantes) * 100;
-                contador2++;
-            }
-            if (votosCandidatoB > votosCandidatoC) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato B con ${candidatoB} votos`;
-            } else if (votosCandidatoC > votosCandidatoB) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato C con ${candidatoC} votos`;
-            } else {
-                document.querySelector("#resultado").innerHTML = "Hay un empate";
-            }
-        } else if (votosCandidatoB > votosCandidatoA && votosCandidatoA > votosCandidatoC) {
-            let votosCandidatoA = 0, votosCandidatoB = 0;
-            while (cantEstudiantes >= contador2) {
-                let voto = prompt("Ingrese el candidato por el que desea votar (A o B)");
-                if (voto == 'A' || voto == 'a') {
-                    candidatoA++;
-                } else if (voto == 'B' || voto == 'b') {
-                    candidatoB++;
-                }
-                votosCandidatoA = (candidatoA / cantEstudiantes) * 100;
-                votosCandidatoB = (candidatoB / cantEstudiantes) * 100;
-                contador2++;
-            }
-            if (votosCandidatoA > votosCandidatoB) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato A con ${candidatoA} votos`;
-            } else if (votosCandidatoB > votosCandidatoA) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato B con ${candidatoB} votos`;
-            } else {
-                document.querySelector("#resultado").innerHTML = "Hay un empate";
-            }
-        } else if (votosCandidatoC > votosCandidatoA && votosCandidatoA > votosCandidatoB) {
-            let votosCandidatoA = 0, votosCandidatoC = 0;
-            while (cantEstudiantes >= contador2) {
-                let voto = prompt("Ingrese el candidato por el que deseea votar (A o C)");
-                if (voto == 'A' || voto == 'a') {
-                    candidatoA++;
-                } else if (voto == 'C' || voto == 'c') {
-                    candidatoC++;
-                }
-                votosCandidatoA = (candidatoA / cantEstudiantes) * 100;
-                votosCandidatoC = (candidatoC / cantEstudiantes) * 100;
-                contador2++
-            }
-            if (votosCandidatoA > votosCandidatoC) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato A con ${candidatoA} votos`;
-            } else if (votosCandidatoC > votosCandidatoA) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato C con ${candidatoC} votos`;
-            } else {
-                document.querySelector("#resultado").innerHTML = "Hay un empate";
-            }
-        } else if (votosCandidatoC > votosCandidatoB && votosCandidatoB > votosCandidatoA) {
-            let votosCandidatoB = 0, votosCandidatoC = 0;
-            while (cantEstudiantes >= contador2) {
-                let voto = prompt("Ingrese el candidato por el que deseea votar (C o B)");
-                if (voto == 'B' || voto == 'b') {
-                    candidatoB++;
-                } else if (voto == 'C' || voto == 'c') {
-                    candidatoC++;
-                }
-                votosCandidatoB = (candidatoB / cantEstudiantes) * 100;
-                votosCandidatoC = (candidatoC / cantEstudiantes) * 100;
-                contador2++;
-            }
-            if (votosCandidatoB > votosCandidatoC) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato B con ${candidatoB} votos`;
-            } else if (candidatoC > candidatoB) {
-                document.querySelector("#resultado").innerHTML = `El ganador es el candidato C con ${candidatoC} votos`;
-            } else {
-                document.querySelector("#resultado").innerHTML = "Hay un empate";
-            }
-        }
+        document.querySelector("#resultado").innerHTML = `La cantidad de autobuses son: ${autobus}`;
+        document.querySelector("#camion").innerHTML = `La cantidad de camiones es: ${camion}`;
+        document.querySelector("#motocicleta").innerHTML = `La cantidad de motocicletas es: ${motocicleta}`;
+        document.querySelector("#turismo").innerHTML = `La cantidad de turismo son ${turismo} y el total de pasajeros es: ${pasajeros}`;
     })
 })
 

3d6d09e8b050ea9cec18645cfd0bd29ca35707b5
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-02 12:50:19 -0500

    Ejercicio06

--- main.js
+++ main.js
@@ -2,23 +2,30 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia3");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let contador = 1, aprobados = 0, reprobados = 0;
-        while(contador <= 2){
-            let nota1 = parseInt(prompt(`Ingrese la primera nota del estudiante ${contador}`));
-            let nota2 = parseInt(prompt(`Ingrese la segunda nota del estudiante ${contador}`));
-            let nota3 = parseInt(prompt(`Ingrese la tercera nota del estudiante ${contador}`));
-            let nota4 = parseInt(prompt(`Ingrese la cuarta nota del estudiante ${contador}`));
-            let nota5 = parseInt(prompt(`Ingrese la quinta nota del estudiante ${contador}`));
-            let promedio = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
-            if(promedio <3){
-                reprobados++;
+        let velAvion = 800 * (1/0.001) * (1/3600) //velocidad inicial del avión en m/s
+        let velProyectil = 0, acelAvion = 20, acelProyectil = 10, reccorridoAvion = 0, recorridoProyectil = 0, distancia = 0, tiempo = 0;
+        while(distancia <= 10000){
+            velAvion += 20;
+            velProyectil += 10;
+            reccorridoAvion = (velAvion * tiempo) + (acelAvion * (tiempo**2)) / 2;
+            recorridoProyectil = (velProyectil * tiempo) + (acelProyectil * (tiempo**2)) /2;
+            distancia = Math.sqrt(reccorridoAvion**2 + recorridoProyectil**2);
+            if(tiempo == 0){
+                alert(`segundo ${tiempo}: Distancia entre el avión y el proyectil: ${distancia.toFixed(2)} m/s`);
+            } else if (tiempo < 10 && 100 < distancia < 1000){
+                alert(`segundo ${tiempo}: Distancia entre el avión y el proyectil: ${distancia.toFixed(2)} m/s`);
+            } else if (tiempo < 10 && 1000 < distancia < 10000){
+                alert(`segundo ${tiempo}: Distancia entre el avión y el proyectil: ${distancia.toFixed(2)} m/s`);
+            } else if (tiempo == 10){
+                alert(`segundo ${tiempo}: Distancia entre el avión y el proyectil: ${distancia.toFixed(2)} m/s`);
+            } else if (tiempo > 10 && distancia < 10000){
+                alert(`segundo ${tiempo}: Distancia entre el avión y el proyectil: ${distancia.toFixed(2)} m/s`);
             } else {
-                aprobados++;
+                alert(`segundo ${tiempo}: Distancia entre el avión y el proyectil: ${distancia.toFixed(2)} m/s`);
+                document.querySelector("#resultado").innerHTML = `Distancia final: segundo ${tiempo} - ${distancia.toFixed(2)} m/s`
             }
-            contador++;
+            tiempo +=1;
         }
-        document.querySelector("#resultado").innerHTML = `Cantidad de alumnos reprobados: ${reprobados}`;
-        document.querySelector("#aprobados").innerHTML = `Cantidad de alumnos aprobados: ${aprobados}`;
     })
 })
 

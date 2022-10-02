d60690245ab53135d21b2380ab49527b43650456
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-02 16:14:12 -0500

    Ejercicio11

--- main.js
+++ main.js
@@ -2,32 +2,40 @@ addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia3");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let vehiculos = document.querySelector("#vehiculos").value;
-        let contador = 1, autobus = 0, camion = 0, motocicleta = 0, turismo = 0, pasajeros = 0;
-        while(vehiculos >= contador){
-            let vehiculo = prompt("Ingrese el tipo de vehiculo");
-            if (vehiculo == "autobus"){
-                autobus++;
-            } else if (vehiculo == "camion"){
-                camion++;
-            } else if (vehiculo == 'motocicleta'){
-                motocicleta++;
-            } else if (vehiculo = 'turismo'){
-                pasajeros += parseInt(prompt("Cantidad de turistas en el vehiculo: "));
-                turismo++;
+        let vehiculos = parseInt(prompt("Ingrese la cantidad de vehículos"));
+        let contador = 1,amarillo = 0,rosa = 0,roja = 0,verde = 0,azul = 0;
+        while(vehiculos>=con){
+            let numeroPlaca=parseInt(prompt("Ingrese el numero de la placa"));
+            if (numeroPlaca == 1 || numeroPlaca==2){
+                amarillo++;
+            } else if (numeroPlaca == 3 || numeroPlaca==4){
+                rosa++;
+            } else if (numeroPlaca == 5 || numeroPlaca==6){
+                roja++;
+            } else if( numeroPlaca ==7 || numeroPlaca ==8){
+                verde++;
+            } else if (numeroPlaca == 9 || numeroPlaca==0){
+                azul++;
+            } else {
+                continue;
             }
             contador++;
         }
-        document.querySelector("#resultado").innerHTML = `La cantidad de autobuses son: ${autobus}`;
-        document.querySelector("#camion").innerHTML = `La cantidad de camiones es: ${camion}`;
-        document.querySelector("#motocicleta").innerHTML = `La cantidad de motocicletas es: ${motocicleta}`;
-        document.querySelector("#turismo").innerHTML = `La cantidad de turismo son ${turismo} y el total de pasajeros es: ${pasajeros}`;
+        document.querySelector("#amarillo").innerHTML = `La cantidad de vehiculos con color amarillo son: ${amarillo}`;
+        document.querySelector("#rosa").innerHTML = `La cantidad de vehiculos con color rosa son: ${rosa}`;
+        document.querySelector("#roja").innerHTML = `La cantidad de vehiculos con color roja son: ${roja}`;
+        document.querySelector("#verde").innerHTML = `La cantidad de vehiculos con color verde son: ${verde}`;
+        document.querySelector("#azul").innerHTML = `La cantidad de vehiculos con color azul son: ${azul}`;
     })
 })
 
 function limpiar() {
     document.querySelector("#guia3").reset();
-    document.querySelector("#resultado").innerHTML = "";
+    document.querySelector("#amarillo").innerHTML = "";
+    document.querySelector("#rosa").innerHTML = "";
+    document.querySelector("#roja").innerHTML = "";
+    document.querySelector("#verde").innerHTML = "";
+    document.querySelector("#azul").innerHTML = "";
 }
 
 addEventListener('reset', limpiar);

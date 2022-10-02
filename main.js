61daa1ac56f9bc68ef23aa38a8e10075b0f427f9
fragoba <franklin.gomez@o365.unab.edu.co>
2022-10-02 11:31:03 -0500

    Ejercicio02

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-function calc(cantidadGallinas) {
-    let contador = 1, calidad = 0;
-    const precio = 500;
-    while (cantidadGallinas >= contador) {
-        let peso = parseInt(prompt(`Ingrese el peso de la gallina ${contador} (en kilos)`));
-        let altura = parseInt(prompt(`Ingrese la altura de la gallina ${contador} (en cm)` ));
-        let huevos = parseInt(prompt(`Ingrese la cantidad de huevos de la gallina ${contador}`));
-        calidad += (peso * altura) / huevos;
-        contador++;
-    }
-    let promedio = calidad / cantidadGallinas;
-    let kilos = parseInt(prompt("Ingrese la cantidad de kilos deseados"));
-    let resultado = (kilos * promedio) * precio;
-    document.querySelector("#resultado").innerHTML = kilos + " kilos: $" + resultado  + " pesos";
-}
+let suma = (numero1, numero2) => numero1 + numero2;
+let resta = (numero1, numero2) => numero1 - numero2;
+let multiplicacion = (numero1, numero2) => numero1 * numero2;
+let division = (numero1, numero2) => numero1 / numero2;
+
 
 addEventListener("DOMContentLoaded", (e) => {
     let calcular = document.querySelector("#guia3");
     calcular.addEventListener("submit", (e) => {
         e.preventDefault();
-        let cantidadGallinas = document.querySelector("#cantidadGallinas").value;
-        calc(cantidadGallinas);
+        let contador = 1;
+        while(contador <= 10){
+            numero1 = parseInt(prompt("Ingrese el primer número"));
+            numero2 = parseInt(prompt("Ingrese el segundo número"));
+            let sumar = suma(numero1, numero2);
+            let restar = resta(numero1, numero2);
+            let multiplicar = multiplicacion(numero1, numero2);
+            let dividir = division(numero1, numero2);
+            alert("La suma de ambos números es: " + sumar);
+            alert("La resta de ambos números es: " + restar);
+            alert("La multiplicación de ambos números es: " + multiplicar);
+            alert("La división de ambos números es: " + dividir);
+            contador++; 
+        }
     })
 })
 
 function limpiar() {
-    document.querySelector("#guia2").reset();
+    document.querySelector("#guia3").reset();
     document.querySelector("#resultado").innerHTML = "";
-    document.querySelector("#subtotal").innerHTML = "";
-    document.querySelector("#iva").innerHTML = "";
 }
 
 addEventListener('reset', limpiar);
